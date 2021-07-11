c31fac1b3962f1d20024d30710f7add4b3bba1c4
CharlieBerens <38572813+Yeah-No@users.noreply.github.com>
2021-07-11 17:27:29 -0600

    Debugging

--- MERN Question Serving Server/config/connect_db.js
+++ MERN Question Serving Server/config/connect_db.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 // Reads files from default.json and attempts to connect to the database
 
 const connectDB = async () => {
+  console.log('=-------------', process.env.DB_URI, process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env)
   try {
-    await mongoose.connect(process.env.db_uri, //URI is stored in .env, this reads it and passes the string
+    await mongoose.connect(process.env.DB_URI, //URI is stored in .env, this reads it and passes the string
     {useNewUrlParser:true, useUnifiedTopology: true}); //Uses a non-deprecated URL parser and other thing
     console.log(`MongoDB is Connected, it\'s grooving time`);
   } catch (err) {
